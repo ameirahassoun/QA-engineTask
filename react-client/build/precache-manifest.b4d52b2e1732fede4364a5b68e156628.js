ba18bcb5c0cd6803ba2125e45c070aaa97da9255
ameirahassoun <afhassoun5@gmail.com>
2018-11-30 19:03:24 +0200

    setup a react project

---
+++ react-client/build/precache-manifest.b4d52b2e1732fede4364a5b68e156628.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f9d910ee606ca2954b2d",
+    "url": "/static/js/main.f9d910ee.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "f9d910ee606ca2954b2d",
+    "url": "/static/css/main.a5f59cef.chunk.css"
+  },
+  {
+    "revision": "e84cc4b8e06d231bb0dfde8f1368d703",
+    "url": "/index.html"
+  }
+];
