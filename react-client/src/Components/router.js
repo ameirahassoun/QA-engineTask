52ca9bedaa1ac4b20ce89580ffe128cef6cfe352
ameirahassoun <afhassoun5@gmail.com>
2018-12-02 23:57:27 +0200

    adding admin router

--- react-client/src/Components/router.js
+++ react-client/src/Components/router.js
@@ -3,15 +3,18 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Main from './mainPage.js';
 import Login from './login.js';
 import Signup from './signup.js';
+import Admin from './admin';
 
 class Router extends React.Component {
     render () {
         return (
             <BrowserRouter>
                 <Switch>
-                    <Route exact path='/' component={Main} />
+                    <Route exact path='/' component={Signup} />
                     <Route exact path='/login' component={Login} />
                     <Route exact path='/signup' component={Signup} />
+                    <Route exact path='/main' component={Main} />
+                    <Route exact path='/admin' component={Admin} />
                 </Switch>
             </BrowserRouter>
         );
